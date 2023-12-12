440ce4ee124ece20a18662adb86dc1b0fe7abc66
viper23 <htonnycarlos@gmail.com>
2023-12-12 21:37:53 +0200

    Add field status to application

--- app/models/application.py
+++ app/models/application.py
@@ -17,6 +17,7 @@ class Application(models.Model) :
     residence_card_number = models.CharField(max_length=20)
     matricule_maquisard = models.CharField(max_length=20, help_text="Si vous n'avez pas de maquisard, ce numéro n'est pas obligatoire. Il est obligatoire si vous en avez.", null=True)
     residence_card_maquisard = models.CharField(max_length=20, help_text="N° de la carte de résidence du Maquisard", null=True)
+    status = models.BooleanField(default=False)
     date_application = models.DateField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
