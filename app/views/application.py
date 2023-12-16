e9d737fee4ceea82f6c07ff409188bca83d82e58
viper23 <htonnycarlos@gmail.com>
2023-12-16 10:47:56 +0200

    Alter field status on application

--- app/views/application.py
+++ app/views/application.py
@@ -110,7 +110,7 @@ def store_application_student(request):
             try:
                 student = Student.objects.get(matricule=student_matricule)
                 if student.matricule == application.matricule:
-                    application.status = "pending"
+                    application.status = 2
                     application.save()
                     messages.success(request, "Votre demande a été envoyée avec succès!")
                 else:
